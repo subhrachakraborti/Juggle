2b366e05795733f0b1ee603b481dabaf7c9b50ce
Subhra Chakraborti <subhrachakraborti@example.com>
2025-11-29 12:04:40 +0530

    Fixing Meta Data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
 
 export const metadata: Metadata = {
-  title: 'Juggle Fin-Service',
+  title: 'Juggle Fintech',
   description: 'Stop juggling finances. Start living.',
 };
 
